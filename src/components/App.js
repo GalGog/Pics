aca70bc4ab7ca54d12dddbb1a367e528e4013f07
GalGog <galynna00@gmail.com>
2019-02-19 10:50:19 +0000

    making API requests with React

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
@@ -6,8 +7,18 @@ import SearchBar from './SearchBar';
 class  App extends React.Component {
 
         onSearchSubmit (term) {
-            console.log(term);
+           axios.get('https://api.unsplash.com/search/photos',{
+               params: { query: term},
+               headers: {
+                   Authorization: 'Client-ID 9ea1c127d2814cd253337bec06dad5ed34c2861a271c59e344e758df668bbff2'
+               }
+           }).then((response) => {
+               console.log(response.data.results);
+        } );
         }
+
+
+
     render (){
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
